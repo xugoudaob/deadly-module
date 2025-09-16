7353fba1580d68bb0250500236f5be453ec86784
xugoudaob <2093289509@qq.com>
2025-09-16 22:25:40 +0800

    更新 deadly.py
    find more bugs

--- deadly/deadly.py
+++ deadly/deadly.py
@@ -1,12 +1,13 @@
-#deadly1.3.4.2-beta by xgdb
+#deadly1.3.4.4-beta by xgdb
 import sys
 import time
-import platform
+#import platform      做一个废案留下的遗产...
 __all__=['deadly','deadly_exit','gua_n','plus','gua','jinz','jz','rpn','caln','guess_num']
 class DeadlyError(Exception):
     def __init__(self, message ="You met an DeadlyError,which means maybe you are Deadly!"):
         super().__init__(message)
         self.message = message
+
 
     def __str__(self):
         return f"{self.message}"
@@ -15,12 +16,12 @@ def deadly(t1='Deadly! '):
     t=''
     if type(t1) != str:
         raise DeadlyError("Maybe the programmer could not know which type on the function")
-        return
+        #return None
     while t!=t1:
         print('deadly')
         t=input('>>> ')
-def deadly_exit():
-    deadly()
+def deadly_exit(t2='Deadly! '):
+    deadly(t2)
     sys.exit()
 def gua_n(l1,l2,num):
     print("注：每一卦形代表一定的事物。乾代表天，坤代表地，巽（xùn）代表风，震代表雷，坎代表水，离代表火，艮（gèn）代表山，兑代表泽。")
@@ -45,11 +46,9 @@ def gua():
     yao={0:'--  --',1:'------'}
     print('现在，口问你有用多少张牌\n当输入"0"时，他会用12张牌，输入"1"时也为12张，"2"时给18张，以此类推。')
     ca=int(input('请输入:'))
-    def card_mk(ca):
+    def card_mk(ca=12):
         if ca<0:
             print('口认为你是个sb，并给你12张牌')
-            card=12
-        elif ca == 0:
             card=12
         else:
             card=ca*6+6
@@ -98,7 +97,7 @@ def gua():
         return b
     ls=nun(seed,card);lst=prch(ls)
     tim=time.time()-tim
-    print("现在，口已经根据你给的字符用了"+str(tim)+"s打乱了牌，并得到了一个牌堆。\n请你为他提供6个不重复的牌号以完成算卦")
+    print("现在，口已经根据你给的字符用了{}}打乱了牌，并得到了一个牌堆。\n请你为他提供6个不重复的牌号以完成算卦".format(tim))
     i=0;lis=[]
     while i<6:
         mber=int(input("第"+str(i+1)+"张(已抽取的:"+str(lis)+")"))
@@ -128,19 +127,19 @@ def gua():
 def jinz():
     from deadly import jz
     x=input("请输入进制数(2～36),没有不输")
-    if x!=None:
+    if not (x is None):
         x=int(x)
-    if not(1<=x<=36 or x==None):
+    if not(1<=x<=36 or x is None):
         raise DeadlyError()
-        return
+        #return None
     elif x==1:
         raise DeadlyError(message="请你回答一下:0的一进制是啥")
-        return
+        #return None
     x=int(x)
     print("进制如何转换？")
-    k=int(input('"0"代表x至10进制，"1"代表10至x进制，"2"代表找进制:'))
+    k=int(input("\"0\"代表{}至10进制，\"1\"代表10至{}进制，\"2\"代表找进制:".format(x,x)))
     if k==0:
-        y=input(f"输入一个{x}进制数:").upper()
+        y=input("输入一个{}进制数:".format(x)).upper()
     elif k==1:
         y=int(input("输入转换成十进制的数:"))
     elif k==2:
@@ -151,9 +150,12 @@ def jinz():
         elif i==1:
             xi=input("数的结果:").upper()
             yi=int(input("该数的进制(如果与之前填写的进制一致则可不填):"))
-            if yi==None:
+            if yi is None:
                 yi=x
             x=jz(1,xi,yi)
+    else:
+        y=0
+        deadly_exit()
     while k!=2 and not x in range(1,37):
         x=int(input("是不是忘了些啥？"))
     m=jz(k,x,y)
@@ -164,8 +166,27 @@ def jz(k,x,y):
     di="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if type(y)==str:
         y.upper()
+    if k==1 or k==0:
+        need = di[:x] #a deadly bug
+    elif k == 2:
+        left, right = 2, 36
+        mid = (left + right) // 2
+        while left <= right:
+            v = jz(1, x, mid)
+            if v == y:
+                p = mid
+                return p
+            elif v < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        p = -1
+        return p
+    else:
+        raise DeadlyError("暂未开放")
+
     if k==0:
-        l=0;need=di[:x]
+        l=0
         for i in y:
             if i not in need:
                 deadly_exit()
@@ -178,31 +199,16 @@ def jz(k,x,y):
         while l!=0:
             k1=l%x;l=l//x
             sfan+=need[k1]
-        s=sfan[::-1];return s
-    elif k==2:
-        left,right=2,36
-        mid=(left+right)//2
-        while left<=right:
-            v=jz(1,x,mid)
-            if v==y:
-                p=mid;return p
-            elif v<x:
-                left=mid+1
-            else:
-                right=mid-1
-        p=-1
-        return p
+        s=sfan[::-1]
+        return s
+    raise DeadlyError("Meet unknown error")
 
 def rpn(user_input=''):#逆波兰表达式
-
     user_input += input("请输入逆波兰表达式：")
-    
     # 替换逗号为空格，然后按空格分割
     tokens = user_input.replace(',', ' ').split()
-    
     # 去除空字符串
     tokens = [token for token in tokens if token.strip()]
-
     caln(tokens)
 
 
@@ -214,7 +220,7 @@ def caln(list_1):
     if not type(list_1)==list:
         return
     op=['+','-','*','/','//','%','^']
-    if list_1[0] in op or list[1] in op:
+    if list_1[0] in op or list_1[1] in op:
         return
     for i in list_1:
         if i in op:
@@ -234,7 +240,7 @@ def caln(list_1):
                 c=a+b
             elif i=='-':
                 c=a-b
-            
+
             st.append(c)
             del c
         else:
@@ -264,7 +270,7 @@ def guess_num(r1 = 0,r2 = 100,fla=False):
             try:
                 r1=int(input("请输入一个有效的小正值"))
                 r2=int(input("请输入一个有效的大正值"))
-            except:
+            except ValueError:
                 print("err......")
                 deadly()
     if r1+1 < r2-1:
